d226c7a51ba37e3ec877fe906dc3adfd8f078c2f
Mayar Karkout <karkout_mayar@hotmail.com>
2020-07-03 15:05:52 +0200

    Add formatting error handling

--- xmlfeed/views.py
+++ xmlfeed/views.py
@@ -1,8 +1,11 @@
+from xml.sax import SAXParseException, SAXException
+
 from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 
 import feedparser
+from lxml import etree
 from requests import exceptions
 
 from xmlfeed.models import ProductFeed
@@ -14,12 +17,20 @@ def index(request):
         url = request.GET.get("url")
         if url is None:
             raise exceptions.MissingSchema
-        testerino = requests.get(url)
 
-        if testerino.status_code is 200:
+        request_url = requests.get(url)
+        if request_url.status_code == 200:
             url = request.GET["url"]
 
             feed = feedparser.parse(url)
+            # validate_xml_scheme(feed)
+
+            if feed.bozo == 1:
+                raise SAXException(msg="")
+
+            if len(feed.entries) == 0:
+                raise ValueError
+
             product_feed = ProductFeed(feed)
             nr_of_items = product_feed.nr_of_items
             nr_of_in_stock_items = product_feed.nr_of_in_stock_items
@@ -34,3 +45,22 @@ def index(request):
         return render(request, 'reader.html', {
             'error_msg': 'Please enter a valid URL.',
         })
+    except ValueError as exception:
+        return render(request, 'reader.html', {
+            'error_msg': 'File formatting error (missing "item" tag).',
+        })
+    except SAXException as exception:
+        return render(request, 'reader.html', {
+            'error_msg': 'The feed data is not well-formed XML.',
+        })
+
+
+# def validate_xml_scheme(feed):
+#     xmlschema_doc = etree.parse('simple.xml')
+#     xmlschema = etree.XMLSchema(xmlschema_doc)
+#
+#     xml_doc = etree.parse('badsample.xml')
+#     result = xmlschema.validate(xml_doc)
+#
+#     return result
+
